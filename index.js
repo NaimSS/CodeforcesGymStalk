baf54233bdee055d4ee2bdd790966722da4a0e4c
Naim Shaikhzadeh <n242275@dac.unicamp.br>
2021-09-11 15:43:49 -0300

    ordenado por data

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ async function sendRequest(url){
     return data;
 }
 
-/*const getData = () => {
-    return sendRequest('GET','https://codeforces.com/api/contest.list?gym=true');
-};*/
 
 function getSubmissions(user){ // ONLY 10 last for debug
     return "https://codeforces.com/api/user.status?handle=" + user;// + "&from=1&count=10";
@@ -15,10 +12,24 @@ function getSubmissions(user){ // ONLY 10 last for debug
 
 //https://codeforces.com/api/contest.status?contestId=566&handle=huangxiaohua
 
+class Contest{
+    constructor(id,name,data){
+        this.id = id;
+        this.name = name;
+        this.data = data;
+    }
+};
+
+function compareContest(a,b){
+    if(a.data > b.data)return -1;
+    if(a.data < b.data)return 1;
+    return 0;
+}
+
 async function getGyms(v,gyms){
     const mapa = new Map();
     const idBom = new Map();
-    
+
     for(let i=0;i<gyms.length;i++){
         idBom.set(gyms[i].id,1);
     }
@@ -32,9 +43,22 @@ async function getGyms(v,gyms){
         await sendRequest(getSubmissions(v[i])).then(
             data =>{
                 if(data.status == 'OK'){
+                    let vet = data.result;
+                    
                     for(let j=0;j<data.result.length;j++){
+                    
                         if(idBom.get(data.result[j].contestId)!=undefined){
-                            mapa.set(data.result[j].contestId,1);
+                            
+                            if(mapa.get(data.result[j].contestId)==undefined){
+                                mapa.set(data.result[j].contestId,data.result[j].creationTimeSeconds);
+                            }
+                            
+                            let curT = mapa.get(data.result[j].contestId);
+                            let nv = vet[j].creationTimeSeconds;
+
+                            if(curT > nv){
+                                mapa.set(data.result[j].contestId,vet[j].creationTimeSeconds);
+                            }
                         }
                     }
                 }
@@ -44,9 +68,21 @@ async function getGyms(v,gyms){
     return mapa;
 }
 
-function gymUrl(id,name){
-    return "<p><a href = " + '"' + "https://codeforces.com/gym/" + id + '"' + ">" + name +"</a><br><p>";
+function pegarDia(data){
+    var a = new Date(data * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var time = date + ' ' + month + ' ' + year;
+    return time;
 }
+
+function gymUrl(data,id,name){
+    return "<p> " + data + ": " + "<a href = " + '"' + "https://codeforces.com/gym/" + id + '"' + ">"+ name +"</a><br></p>";
+}
+
+
 
 function main(gyms){
 
@@ -61,6 +97,8 @@ function main(gyms){
         nomes.set(obj.id,obj.name);
     }
 
+    let vec = [];
+    let tam = 0;
     getGyms(v1,gyms.result).then(
         mapa1 =>{
             getGyms(v2,gyms.result).then(
@@ -72,8 +110,17 @@ function main(gyms){
 
                         if(mapa2.get(key)==undefined){
                             // nice :)
-                            document.write(gymUrl(key,nomes.get(key)));
+                            //document.write(gymUrl(key,nomes.get(key)));
+                            vec[tam] = new Contest(key,nomes.get(key),value);
+                            tam++;
                         }
+                    }
+
+                    vec.sort(compareContest);
+                    document.getElementById('PLZ').innerHTML = "";
+                    document.write("<h1> Found " +  vec.length + " gyms </h1>");
+                    for(let i=0;i<vec.length;i++){
+                        document.write(gymUrl(pegarDia(vec[i].data),vec[i].id,vec[i].name));
                     }
 
                 }
@@ -87,13 +134,11 @@ async function retornaGyms(){
     return await sendRequest('https://codeforces.com/api/contest.list?gym=true');
 }
 function runScript(){ 
-    document.writeln("Please Wait");
+    document.write('<p id = "PLZ">Please Wait<\p>');
     
     retornaGyms().then(
         gyms =>{
-            console.log("entra");
             main(gyms);
-            console.log("sai");
         }
     );
 }
